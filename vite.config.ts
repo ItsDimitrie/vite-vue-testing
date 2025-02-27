f2da1aba31cc3f4db946c078148795f539260a59
Dimitrie Lauer <112709753+ItsDimitrie@users.noreply.github.com>
2025-02-27 09:29:03 +0100

    Changed URL of project in vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vite-vue-testing',
+  base: '/Table.One-Embed',
   plugins: [vue(), tailwindcss()],
 })
